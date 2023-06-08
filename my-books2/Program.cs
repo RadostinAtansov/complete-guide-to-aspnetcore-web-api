c25605fcdc688314ed5b430478089c1036a70370
Radostin <radstatan@gmail.com>
2023-06-08 19:08:17 +0300

    test project for studying

--- my-books2/Program.cs
+++ my-books2/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using my_books2.Data;
+using my_books2.Data.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//
+var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+                                                                //Server=.;Database=myDataB;Trusted_Connection=True;
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
+
+//configure the services
+builder.Services.AddTransient<BooksService>();
+builder.Services.AddTransient<AuthorsService>();
+builder.Services.AddTransient<PublisherService>();
 
 var app = builder.Build();
 
@@ -22,4 +37,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+//AppDbInitializer.Seed(app);
+
+app.Run(); 
